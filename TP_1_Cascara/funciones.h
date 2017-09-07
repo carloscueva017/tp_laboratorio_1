067af09fbe71c5c95254a2887bc91c2e36644c68
carloscueva017@hotmail.com <carloscueva017@hotmail.com>
2017-09-07 19:51:32 -0300

    TRABAJO PRACTICO 1

---
+++ TP_1_Cascara/funciones.h
@@ -0,0 +1,47 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+    /** \brief esta funcion Suma dos numeros
+     *
+     * \param toma el numero 1
+     * \param toma el numero 2
+     * \return devuelve el resultado
+     *
+     */
+        int suma(int num1,int num2);
+
+    /** \brief esta funcion resta dos numeros
+     *
+     * \param toma el numero 1
+     * \param toma el numero 2
+     * \return devuelve el resultado
+     *
+     */
+        int resta(int num1,int num2);
+
+    /** \brief esta funcion divide dos numeros enteros
+     *
+     * \param toma el numero 1
+     * \param toma el numero 2
+     * \return devuelve un resultado flotante
+     *
+     */
+        float division(float num1,float num2);
+
+    /** \brief esta funcion multiplica dos numeros
+     *
+     * \param toma el numero 1
+     * \param toma el numero 2
+     * \return devuelve el resultado
+     *
+     */
+        int multiplicacion(int num1,int num2);
+
+    /** \brief esta funcion factoriza un numero
+     *
+     * \param toma el numero 1
+     * \return devuelve el resultado
+     *
+     */
+        int factorial(int num1);
+
+#endif // FUNCIONES_H_INCLUDED
